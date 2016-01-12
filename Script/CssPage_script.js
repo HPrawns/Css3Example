83cbf955a63ee8751d89e90daae612f5d6b1f13f
HPrawns <sgdb999@163.com>
2016-01-12 11:41:17 +0800

    2015-1-12上午
    构筑基本项目框架包含文件夹结构
    基本样式和脚本文件
    加入第一个CSS事例

---
+++ Script/CssPage_script.js
@@ -0,0 +1,7 @@
+﻿//第一个示例样式  初始化开始
+//percentage:宽度/百分比    timeinterval:完成时间/毫秒
+function Schedule_textInit(percentage, timeinterval) {
+    $(".schedule_text_d1").animate({
+        width: percentage + "%"
+    }, timeinterval);
+}
